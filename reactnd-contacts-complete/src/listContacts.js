3abdf946e5295ccfa9c49346b66f0a31b73264a6
JayChenFE <chenjiehwy@163.com>
2017-09-22 22:21:53 +0800

    2

--- reactnd-contacts-complete/src/listContacts.js
+++ reactnd-contacts-complete/src/listContacts.js
@@ -19,13 +19,23 @@ class ListContacts extends Component {
     })
   }
 
+  clearQuery = _ => {
+    this.setState({
+      query: ''
+    })
+  }
+
   render() {
+
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
+
     let showingContacts
-    if (this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      showingContacts = this.props.contacts.filter(contact => match.test(contact.name))
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingContacts = contacts.filter(contact => match.test(contact.name))
     } else {
-      showingContacts = this.props.contacts
+      showingContacts = contacts
     }
     showingContacts.sort(sortBy('name'))
 
@@ -35,9 +45,19 @@ class ListContacts extends Component {
           <input type="text"
             className="search-contacts"
             placeholder='Search Contacts'
-            value={this.state.query}
+            value={query}
             onChange={e => this.updateQuery(e.target.value)} />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>
+              Show all
+            </button>
+          </div>
+        )}
+
         <ol className="contact-list">
           {showingContacts.map(contact => (
             <li key={contact.id} className='contact-list-item'>
@@ -49,7 +69,7 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button className="contact-remove" onClick={() => this.props.onDeleteContact(contact)}>
+              <button className="contact-remove" onClick={() => onDeleteContact(contact)}>
                 remove
           </button>
             </li>
